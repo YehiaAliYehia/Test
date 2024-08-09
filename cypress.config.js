281b68fa45baf00f7bf1e4e73bd7c7911e180343
YehiaAliYehia <yehia.ali@nana.sa>
2024-08-09 03:46:49 +0300

    Add projectId to Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'vhri3r',  // This is your Cypress project ID
+  projectId: 'hy2cdj',  // This is your Cypress project ID
   e2e: {
     baseUrl: 'https://automationexercise.com',  // This is the base URL for your tests
   },
